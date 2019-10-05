6f0f91bdb8803ed0b371949a78c551d149c8d5e4
Dane <arpinoda@gmail.com>
2019-10-05 14:37:32 -0400

    Removed old, unused env variable.

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,5 @@
 const merge = require('webpack-merge');
 const TerserPlugin = require('terser-webpack-plugin');
-const webpack = require('webpack');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
@@ -9,9 +8,4 @@ module.exports = merge(common, {
     minimize: true,
     minimizer: [new TerserPlugin()],
   },
-  plugins: [
-    new webpack.DefinePlugin({
-      API_URL: JSON.stringify(process.env.API_URL),
-    }),
-  ],
 });
