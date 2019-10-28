c1bea12cccc4721b63766e6fdc4569e55d8f6d87
Dane <arpinoda@gmail.com>
2019-10-28 11:43:17 -0400

    Tweaks

--- src/components/ProjectDetail.js
+++ src/components/ProjectDetail.js
@@ -104,7 +104,7 @@ class ProjectDetail extends React.Component {
             position: 'absolute',
             background: '#fff',
             top: 25,
-            maxWidth: 850,
+            maxWidth: 750,
             zIndex: 2,
             border: '2px solid #444',
             left: 0,
