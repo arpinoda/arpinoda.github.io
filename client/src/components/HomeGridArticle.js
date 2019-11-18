3e9f047bb83c81a3d9f97e5e452b702cf9abe2b3
Dane <arpinoda@gmail.com>
2019-11-18 12:32:23 -0500

    Sticky headers complete

--- client/src/components/HomeGridArticle.js
+++ client/src/components/HomeGridArticle.js
@@ -3,11 +3,21 @@ import PropTypes from 'prop-types';
 import HomeGridFigure from './HomeGridFigure';
 
 const HomeGridArticle = ({ category, ...props }) => {
-  const divClassName = `col-12 ${category.className ? category.className : ''}`;
+  const divClassName = `col-12 bg-white ${
+    category.className ? category.className : ''
+  }`;
+  const headings = category.breadcrumbs.split('>');
+  const hidden = headings.slice(0, headings.length - 1);
+
   return (
     <article id={category.elementID}>
       <div className={divClassName}>
-        <h3 className="pt2 pb2 h3">{category.breadcrumbs}</h3>
+        <h3 className="pt2 pb2 h3">
+          {hidden.map(title => (
+            <span className="white" key={title}>{`${title} > `}</span>
+          ))}
+          {headings[headings.length - 1]}
+        </h3>
       </div>
       <div className="projects flex flex-wrap">
         {category.projects &&
