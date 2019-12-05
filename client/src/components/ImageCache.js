5efb93ab7483949cebcb775b34f62555d2294ec7
Dane <arpinoda@gmail.com>
2019-12-04 23:21:33 -0500

    Reduced network requests by storing pending promise

--- client/src/components/ImageCache.js
+++ client/src/components/ImageCache.js
@@ -16,22 +16,34 @@ const blobUrl = blob => {
 const api = new API({ url: process.env.API_PATH });
 api.createEntity({ name: 'image' });
 const blobs = new Map();
+// blobs[key] = [promise, url]
+
+// store key, promise, resolved value
 
 const ImageCache = id => {
   const existingValue = blobs.get(id);
 
   if (existingValue !== undefined) {
-    console.log('exists!');
-    return new Promise(existingValue);
+    const promise = existingValue[0];
+    const url = existingValue[1];
+
+    if (url) {
+      return new Promise(url);
+    }
+
+    return promise;
   }
 
-  return api.endpoints.image.getOne(id).then(res =>
+  const promise = api.endpoints.image.getOne(id).then(res =>
     res.blob().then(blob => {
       const url = blobUrl(blob);
-      blobs.set(id, url);
+      blobs.set(id, [promise, url]);
       return url;
     }),
   );
+
+  blobs.set(id, [promise, null]);
+  return promise;
 };
 
 export default ImageCache;
