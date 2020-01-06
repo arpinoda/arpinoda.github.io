6598e34945301d5174a53c165c28cce818f40255
Dane <arpinoda@gmail.com>
2020-01-06 11:30:44 -0500

    Updated welcome padding top

--- client/src/components/LoginWelcome.js
+++ client/src/components/LoginWelcome.js
@@ -1,31 +1,16 @@
 import React from 'react';
-import ResponsiveLayout from './ResponsiveLayout';
 
-const LoginWelcome = () => {
-  const content = (
-    <>
-      <div className="h1 mb1 bold">
-        Welcome
-        <span className="h1" aria-label="Waving hand hello!" role="img">
-          {' '}
-          👋
-        </span>
-      </div>
-      <div>Sign in with your passcode below.</div>
-    </>
-  );
-
-  return (
-    <ResponsiveLayout
-      breakpoint={500}
-      renderDesktop={() => (
-        <div className="center" style={{ marginTop: 'calc(50vh - 200px)' }}>
-          {content}
-        </div>
-      )}
-      renderMobile={() => <div className="center mt3">{content}</div>}
-    />
-  );
-};
+const LoginWelcome = () => (
+  <div className="center" style={{ paddingTop: 'calc(40vh - 135px)' }}>
+    <div className="h1 mb1 bold">
+      Welcome
+      <span className="h1" aria-label="Waving hand hello!" role="img">
+        {' '}
+        👋
+      </span>
+    </div>
+    <div>Sign in with your passcode below.</div>
+  </div>
+);
 
 export default LoginWelcome;
